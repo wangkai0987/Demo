257a912803a4293425951e0bcfdb2b14eacd56f7
wangkai0987 <52160353+wangkai0987@users.noreply.github.com>
2020-03-31 16:15:10 +0800

    2020.3.30

--- src/main/java/com/mysample/controller/MyController.java
+++ src/main/java/com/mysample/controller/MyController.java
@@ -1,8 +1,13 @@
 package com.mysample.controller;
 
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,11 +17,30 @@ public class MyController {
     @RequestMapping("invokeMethod")
     @ResponseBody
     public Map<String, Object> invokeMethod(@RequestParam(value="Action") String Action,
-                               @RequestBody String jsonBody) {
+                               @RequestBody String jsonBody) throws IOException {
 
         Map<String, Object> map = new HashMap<>();
 
         map.put("result", "complete");
+        map.put("Action", Action);
+        map.put("jsonBody", jsonBody);
+
+        //
+
+//        CloseableHttpClient httpclient = HttpClients.createDefault();
+//        HttpGet httpget = new HttpGet("http://localhost/");
+//        CloseableHttpResponse response = httpclient.execute(httpget);
+//        try {
+//
+//            // TODO
+//
+//        } finally {
+//
+//            response.close();
+//
+//        }
+
+        //
 
         return map;
 
